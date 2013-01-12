3094832d61dbaec26974d8beac1a2805533238ca
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-01-12 19:40:02 +0000

    clean up file io a little

--- lib/pi_piper/pin.rb
+++ lib/pi_piper/pin.rb
@@ -8,13 +8,13 @@ module PiPiper
       @invert = options[:invert].nil? ? false : options[:invert]
      
       File.open("/sys/class/gpio/export", "w") { |f| f.write("#{@pin}") }
-      File.open("/sys/class/gpio/gpio#{pin}/direction", "w") { |f| f.write(@direction == :out ? "out" : "in") }
+      File.open(direction_file, "w") { |f| f.write(@direction == :out ? "out" : "in") }
       
       @last_value = value
     end
     
     def on
-      File.open(filename, 'w') {|f| f.write("1") } if direction == :out
+      File.open(value_file, 'w') {|f| f.write("1") } if direction == :out
     end
     
     def on?
@@ -22,7 +22,7 @@ module PiPiper
     end
     
     def off
-      File.open(filename, 'w') {|f| f.write("0") } if direction == :out
+      File.open(value_file, 'w') {|f| f.write("0") } if direction == :out
     end
     
     def off?
@@ -34,21 +34,29 @@ module PiPiper
     end
 
     def wait_for_change
-      fd = File.open(filename, "r")
-      File.open("/sys/class/gpio/gpio#{pin}/edge", "w") { |f| f.write("both") }
+      fd = File.open(value_file, "r")
+      File.open(edge_file, "w") { |f| f.write("both") }
       fd.read
       IO.select(nil, nil, [fd], nil)
       true
     end
     
     def value
-      val = File.read(filename).to_i
+      val = File.read(value_file).to_i
       invert ? (val ^ 1) : val
     end
     
     private
-    def filename
+    def value_file
       "/sys/class/gpio/gpio#{pin}/value"
+    end
+
+    def edge_file
+      "/sys/class/gpio/gpio#{pin}/edge"
+    end
+
+    def direction_file
+      "/sys/class/gpio/gpio#{pin}/direction"
     end
   
   end
