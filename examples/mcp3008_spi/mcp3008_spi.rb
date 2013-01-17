2ead95513b3df9a54acef3e78151243b4d56d8d2
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-01-17 04:21:59 +0000

    WIP, porting mcp3008 example to use SPI

---
+++ examples/mcp3008_spi/mcp3008_spi.rb
@@ -0,0 +1,23 @@
+require 'pi_piper'
+
+adc_num =0 
+
+loop do
+  value = 0
+  PiPiper::Spi.begin do |spi|
+    spi.clock(PiPiper::Spi::CLOCK_DIVIDER_512)
+    raw = spi.write 0x1, 0x80, 0x0
+    value = ((raw[1]&3) << 8) + raw[2]
+  end
+
+  invert = 1023 - value
+  mvolts = invert * (3300.0 / 1023.0)
+  if mvolts < 2700
+    temp = (mvolts - 380.0) / (2320.0 / 84.0)
+  else
+    temp = (mvolts - 2700.0) / (390.0 / 92.0) + 84.0
+  end
+  temp_f = (temp * 9.0 / 5.0) + 32
+  puts "Value = #{value}, invert = #{invert}, mvolts = #{mvolts}, temp = #{temp} C | #{temp_f} F"
+  sleep 1
+end
