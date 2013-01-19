a4f2a02c28407f77bc900fd9ad0eca363df13518
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-01-19 04:34:07 +0000

    export the bcm2835_spi_transfernb method

--- lib/pi_piper/bcm2835.rb
+++ lib/pi_piper/bcm2835.rb
@@ -11,6 +11,7 @@ module PiPiper
     attach_function :spi_begin,       :bcm2835_spi_begin,            [], :uint8
     attach_function :spi_end,         :bcm2835_spi_end,              [], :uint8
     attach_function :spi_transfer,    :bcm2835_spi_transfer,         [:uint8], :uint8
+    attach_function :spi_transfernb,  :bcm2835_spi_transfernb,       [:string, :string, :uint], :void
     attach_function :spi_clock,       :bcm2835_spi_setClockDivider,  [:uint8], :void
     attach_function :spi_bit_order,   :bcm2835_spi_setBitOrder,      [:uint8], :void
     attach_function :spi_chip_select, :bcm2835_spi_chipSelect,       [:uint8], :void
