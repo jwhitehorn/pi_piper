fcd03bc9a9a1813264dd8329844695e75d6cfc0d
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-01-22 04:53:08 +0000

    documentation for the PiPiper module

--- lib/pi_piper.rb
+++ lib/pi_piper.rb
@@ -6,6 +6,12 @@ PiPiper::Bcm2835.init
 
 module PiPiper
 
+  #Defines an event block to be executed when an pin event occurs.
+  #
+  # == Parameters:
+  # options:
+  #   Options hash. Options include `:pin`, `:invert` and `:trigger`.
+  # 
   def PiPiper.watch(options)
     Thread.new do
       pin = PiPiper::Pin.new(options)
@@ -16,6 +22,7 @@ module PiPiper
     end.abort_on_exception = true  
   end
 
+  #Prevents the main thread from exiting. Required when using PiPiper.watch
   def PiPiper.wait
     loop do sleep 1 end
   end
