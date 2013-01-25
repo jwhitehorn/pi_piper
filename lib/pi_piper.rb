67352eff7eef6573a5ba9cdc92b80b0346663e13
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-01-25 13:42:06 -0600

    DSL fixes

--- lib/pi_piper.rb
+++ lib/pi_piper.rb
@@ -5,26 +5,30 @@ end
 PiPiper::Bcm2835.init
 
 module PiPiper
-
+  extend self
+  
   #Defines an event block to be executed when an pin event occurs.
   #
   # == Parameters:
   # options:
   #   Options hash. Options include `:pin`, `:invert` and `:trigger`.
   # 
-  def PiPiper.watch(options, &block)
+  def watch(options, &block)
     Thread.new do
       pin = PiPiper::Pin.new(options)
       loop do
         pin.wait_for_change 
-        pin.instance_eval { block.call pin } 
-        #yield pin
+        if block.arity > 0
+          pin.instance_exec &block
+        else
+          block.call pin
+        end
       end 
     end.abort_on_exception = true  
   end
 
   #Prevents the main thread from exiting. Required when using PiPiper.watch
-  def PiPiper.wait
+  def wait
     loop do sleep 1 end
   end
 
