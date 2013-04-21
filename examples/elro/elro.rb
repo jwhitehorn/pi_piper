30e763ff58e19bf526caed282ec2bf0069febfeb
Torsten Mangner <tmangner@inoxio.de>
2013-04-21 20:20:20 +0000

    leaner methods renamed methods to 'sequence'

--- examples/elro/elro.rb
+++ examples/elro/elro.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby
-
 require 'pi_piper'
 
 class RemoteSwitch
@@ -12,6 +11,7 @@ class RemoteSwitch
   # key: according to dipswitches on your Elro receivers
   # pin: according to Broadcom pin naming
   def initialize(device, key=[1,1,1,1,1], pin=17)
+    raise ArgumentError.new("key has to be of size 5") if key.size != 5
     @key = key
     @device = device
     @pin = PiPiper::Pin.new(:pin => pin, :direction => :out)
@@ -27,9 +27,8 @@ class RemoteSwitch
 
   def _switch(switch)
     sequence = []
-
-    sequence << bits_for_key
-    sequence << bits_for_device
+    sequence << sequence_for_key
+    sequence << sequence_for_device
     sequence << (switch ? [DIP_ON, DIP_OFF] : [DIP_OFF, DIP_ON])
     sequence << [128, 0, 0, 0]
 
@@ -38,38 +37,26 @@ class RemoteSwitch
 
 private
   
-  def bits_for_key
-    sequence = []
-    0.upto(4) do |i|
-      sequence << ((@key[i] == 1) ? DIP_ON : DIP_OFF)
-    end
-    sequence
+  def sequence_for_key
+    @key.map { |dip| (dip == 1) ? DIP_ON : DIP_OFF }
   end
 
-  def bits_for_device
-    bits = convert_to_bits(@device, 5)
-    bits.reverse.map {|b| b ? DIP_ON : DIP_OFF}
+  def sequence_for_device
+    convert_to_bits(@device, 5).reverse.map { |b| b ? DIP_ON : DIP_OFF }
   end
 
   def pulses_from_sequence(sequence)
-    pulses = []
-    sequence.each do |part|
-      bits = convert_to_bits(part, 8)
-      pulses.push(*bits)
-    end
-    pulses
+    sequence.map { |part| convert_to_bits(part, 8) }.flatten
   end
 
   def send_pulses(pulses)
     @pin.off
     start_time = Time.now
     REPEAT.times do
-      pulses.each do |b|
-        @pin.update_value(b)
-      end
+      pulses.each { |b| @pin.update_value(b) }
     end
     end_time = Time.now
-    puts "took %.0f microsecs per pulse" % ((end_time - start_time) / (10 * 16 * 8) * 1_000_000)
+    puts "avg %.0f microsecs per pulse" % ((end_time - start_time) / (10 * 16 * 8) * 1_000_000)
   end
 
   def convert_to_bits(num, length)
@@ -82,4 +69,3 @@ if (!ARGV[0])
   exit(0)
 end
 RemoteSwitch.new(ARGV[0].to_i, [0,0,0,0,1], 17)._switch(ARGV[1] == "1")
-
