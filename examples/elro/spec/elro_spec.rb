d0e08b57b8996347b1924516d99fd9fa69371d3b
Torsten Mangner <tmangner@inoxio.de>
2013-04-23 22:01:46 +0200

    more tests

--- examples/elro/spec/elro_spec.rb
+++ examples/elro/spec/elro_spec.rb
@@ -6,4 +6,13 @@ describe ElroSwitch do
     expect {ElroSwitch.new(1, [0,0,1,0,1,0], nil)}.to raise_error ArgumentError
   end
 
+  it "sends pulses on switch" do
+    pin = double("pi_piper_pin")
+    pin.stub(:off)
+    pin.should_receive(:update_value).exactly(16 * 8 * 10).times
+
+    elro = ElroSwitch.new(1, [0,0,1,0,1], pin)
+    elro.switch(true)
+  end
+
 end
