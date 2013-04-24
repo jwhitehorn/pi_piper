0575c9b7dc00973ea4cf6ae2be15d1e81cf636c5
Torsten Mangner <tmangner@inoxio.de>
2013-04-24 09:43:04 +0200

    introduce verbose switch to be less chatty

--- examples/elro/lib/elro_util.rb
+++ examples/elro/lib/elro_util.rb
@@ -24,14 +24,16 @@ module ElroUtil
     sequence.map { |part| ElroUtil.convert_to_bits(part, 8) }.flatten
   end
 
-  def ElroUtil.send_pulses(pin, pulses)
+  def ElroUtil.send_pulses(pin, pulses, verbose=false)
     pin.off
     start_time = Time.now
     REPEAT.times do
       pulses.each { |b| pin.update_value(b) }
     end
     end_time = Time.now
-    puts "avg %.0f microsecs per pulse" % ((end_time - start_time) / (10 * 16 * 8) * 1_000_000)
+    if verbose
+      puts "avg %.0f microsecs per pulse" % ((end_time - start_time) / (10 * 16 * 8) * 1_000_000)
+    end
   end
 
   def ElroUtil.convert_to_bits(num, length)
