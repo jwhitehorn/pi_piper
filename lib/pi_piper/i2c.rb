1dfaa99cd86c0ff74fc620e303d2057a6a4f73d6
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-08-24 05:38:27 +0000

    begin i2c block

--- lib/pi_piper/i2c.rb
+++ lib/pi_piper/i2c.rb
@@ -11,6 +11,15 @@ module PiPiper
       Bcm2835.i2c_end
     end
 
+    def self.begin(&block)
+      instance = I2C.new
+      if block.arity > 0
+        block.call instance
+      else
+        instance.instance_exec &block 
+      end
+    end
+
   end
 
 end
