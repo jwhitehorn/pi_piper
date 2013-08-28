3b4b22570af698e99d46892f21cdb8cb6434d8b7
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-08-28 03:32:41 +0000

    switch to stubs & clean up

--- spec/i2c_spec.rb
+++ spec/i2c_spec.rb
@@ -1,22 +1,30 @@
 require '../pi_piper/lib/pi_piper.rb'
+require 'stub_driver.rb'
 include PiPiper
 
 describe 'pi_piper' do
   describe "when in i2c block" do
 
-    it "should call i2c_begin & end on platform driver" do
-      driver = double "driver"
-      expect(driver).to receive(:i2c_begin)
-      expect(driver).to receive(:i2c_end)
+    it "should call i2c_begin" do
+      driver = StubDriver.new                                                                  
+      expect(driver).to receive(:i2c_begin)  
 
       Platform.driver = driver
       I2C.begin do
       end
     end
 
+    it "should call i2c_end" do                                     
+      driver = StubDriver.new                                                                  
+      expect(driver).to receive(:i2c_end)  
+
+      Platform.driver = driver                                                                 
+      I2C.begin do                                                                             
+      end                                                                                      
+    end 
+
     it "should call i2c_end even after raise" do
-      driver = double "driver"
-      expect(driver).to receive(:i2c_begin)
+      driver = StubDriver.new 
       expect(driver).to receive(:i2c_end)
 
       Platform.driver = driver
@@ -24,7 +32,6 @@ describe 'pi_piper' do
         I2C.begin { raise "OMG" }
       rescue
       end
-
     end
 
   end
---
+++ spec/stub_driver.rb
@@ -0,0 +1,7 @@
+class StubDriver
+
+  def method_missing(meth, *args, &block)
+    #n10did
+  end
+
+end
