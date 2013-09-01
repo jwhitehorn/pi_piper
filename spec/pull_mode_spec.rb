f732e813dcfcbce94cb92de529004dd0d9189d47
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-08-31 22:55:19 -0500

    wire up stub driver... tests failing now, not crashing

--- spec/pull_mode_spec.rb
+++ spec/pull_mode_spec.rb
@@ -1,12 +1,9 @@
-require 'minitest/autorun'
-require 'pi_piper'
-
-
-def is_raspberry?
-  RUBY_PLATFORM == "arm-linux-eabihf"
-end
+require '../pi_piper/lib/pi_piper.rb'
+require 'stub_driver.rb'
+include PiPiper
 
 describe 'Pi_Piper' do
+  Platform.driver = StubDriver.new
   describe "when a pin is created" do
     it "should restrict allowed :pull values" do
       proc { PiPiper::Pin.new(:pin => 17, :direction => :in, :pull => :wth) }.must_raise RuntimeError
@@ -32,7 +29,6 @@ describe 'Pi_Piper' do
   describe "when pull mode is set to up" do
     @pin = PiPiper::Pin.new(:pin => 17, :direction => :in, :pull => :up)
     it "must respond HIGH when floating pins are checked" do
-      skip("Pull-up test run only on Raspberry") unless is_raspberry?
       @pin.on?.must_be true
     end
   end
@@ -40,7 +36,6 @@ describe 'Pi_Piper' do
   describe "when pull mode is set to down" do
     @pin = PiPiper::Pin.new(:pin => 17, :direction => :in, :pull => :down)
     it "must respond LOW when flaoting pins are checked" do
-      skip("Pull-up test run only on Raspberry") unless is_raspberry?
       @pin.off?.must_be true
     end
   end
