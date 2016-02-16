33f436be4314d90f5d1b3495ec640139b8838591
El Matou <elmatou@gmail.com>
2016-02-16 20:17:15 +0100

    Clean Spi specs

--- spec/spi_spec.rb
+++ spec/spi_spec.rb
@@ -1,22 +1,23 @@
 require_relative 'spec_helper'
 
 describe 'Spi' do
+
+  around(:example) do |example|
+    Platform.driver = StubDriver.new.tap do |d|
+        example.run
+     end
+  end
+
   describe 'when in block' do
     it 'should call spi_begin' do
-      driver = StubDriver.new
-      expect(driver).to receive(:spi_begin)
-
-      Platform.driver = driver
-      Spi.begin do
-      end
+      expect(Platform.driver).to receive(:spi_begin)
+      Spi.begin {}
     end
 
     it 'should call spi_chip_select to set and unset chip' do
-      driver = StubDriver.new
-      expect(driver).to receive(:spi_chip_select).with(Spi::CHIP_SELECT_1)
-      expect(driver).to receive(:spi_chip_select).with(Spi::CHIP_SELECT_NONE)
+      expect(Platform.driver).to receive(:spi_chip_select).with(Spi::CHIP_SELECT_1)
+      expect(Platform.driver).to receive(:spi_chip_select).with(Spi::CHIP_SELECT_NONE)
 
-      Platform.driver = driver
       Spi.begin(Spi::CHIP_SELECT_1) do
         read
       end
@@ -25,19 +26,14 @@ describe 'Spi' do
 
   describe 'set mode' do
     it 'should call spi_set_data_mode' do
-      driver = StubDriver.new
-      expect(driver).to receive(:spi_set_data_mode).with(Spi::SPI_MODE3)
-
-      Platform.driver = driver
+      expect(Platform.driver).to receive(:spi_set_data_mode).with(Spi::SPI_MODE3)
       Spi.set_mode(1, 1)
     end
   end
 
   describe '#spidev_out' do
     it 'should attempt to write data to spi' do
-      driver = StubDriver.new
-      expect(driver).to receive(:spidev_out).with([0, 1, 2, 3, 4, 5])
-      Platform.driver = driver
+      expect(Platform.driver).to receive(:spidev_out).with([0, 1, 2, 3, 4, 5])
       Spi.spidev_out([0, 1, 2, 3, 4, 5])
     end
   end
