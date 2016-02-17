c84d1492aedd685eae954a0d18f8c2a1842aa08d
El Matou <elmatou@gmail.com>
2016-02-17 13:43:56 +0100

    Cleanup I2C specs

--- spec/i2c_spec.rb
+++ spec/i2c_spec.rb
@@ -1,57 +1,44 @@
 require_relative 'spec_helper'
 
 describe 'I2C' do
+
+  around(:example) do |example|
+    Platform.driver = StubDriver.new.tap do |d|
+        example.run
+     end
+  end
+  
   describe 'clock setting' do
     it 'should check driver settings' do
-      Platform.driver = StubDriver.new.tap do |d|
-        expect(d).to receive(:i2c_allowed_clocks).and_return([100.kilohertz])
-      end
-
+      expect(Platform.driver).to receive(:i2c_allowed_clocks).and_return([100.kilohertz])
       I2C.clock = 100.kilohertz
     end
 
     it 'should accept 100 kHz' do
-      Platform.driver = StubDriver.new.tap do |d|
-        expect(d).to receive(:i2c_allowed_clocks).and_return([100.kilohertz])
-        expect(d).to receive(:i2c_set_clock).with(100.kilohertz)
-      end
-
+      expect(Platform.driver).to receive(:i2c_allowed_clocks).and_return([100.kilohertz])
+      expect(Platform.driver).to receive(:i2c_set_clock).with(100.kilohertz)
       I2C.clock = 100.kilohertz
     end
 
     it 'should not accept 200 kHz' do
-      Platform.driver = StubDriver.new.tap do |d|
-        expect(d).to receive(:i2c_allowed_clocks).and_return([100.kilohertz])
-      end
-
+      expect(Platform.driver).to receive(:i2c_allowed_clocks).and_return([100.kilohertz])
       expect { I2C.clock = 200.kilohertz }.to raise_error(RuntimeError)
     end
   end
 
   describe 'when in block' do
     it 'should call i2c_begin' do
-      driver = StubDriver.new
-      expect(driver).to receive(:i2c_begin)
-
-      Platform.driver = driver
-      I2C.begin do
-      end
+      expect(Platform.driver).to receive(:i2c_begin)
+      I2C.begin {}
     end
 
     it 'should call i2c_end' do
-      driver = StubDriver.new
-      expect(driver).to receive(:i2c_end)
-
-      Platform.driver = driver
-      I2C.begin do
-      end
+      expect(Platform.driver).to receive(:i2c_end)
+      I2C.begin {}
     end
 
     it 'should call i2c_end even after raise' do
-      driver = StubDriver.new
-      expect(driver).to receive(:i2c_end)
-
-      Platform.driver = driver
+      expect(Platform.driver).to receive(:i2c_end)
       begin
         I2C.begin { raise 'OMG' }
       rescue
@@ -60,20 +47,14 @@ describe 'I2C' do
 
     describe 'write operation' do
       it 'should set address' do
-        Platform.driver = StubDriver.new.tap do |d|
-          expect(d).to receive(:i2c_set_address).with(4)
-        end
-
+        expect(Platform.driver).to receive(:i2c_set_address).with(4)
         I2C.begin do
           write to: 4, data: [1, 2, 3, 4]
         end
       end
 
       it 'should pass data to driver' do
-        Platform.driver = StubDriver.new.tap do |d|
-          expect(d).to receive(:i2c_transfer_bytes).with([1, 2, 3, 4])
-        end
-
+        expect(Platform.driver).to receive(:i2c_transfer_bytes).with([1, 2, 3, 4])
         I2C.begin do
           write to: 4, data: [1, 2, 3, 4]
         end
